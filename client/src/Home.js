d5387b083f30c344bc6e3f6f2658f848f9e499b1
CcliqueE <lasholdingllc@gmail.com>
2021-05-05 23:30:42 -0700

    hamburger icon at a certain width 1.0

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,34 +2,39 @@ import './App.css';
 import React from 'react'
 import Logo from './img/server-logo.png'
 
-import { Navbar, Nav, Form, InputGroup, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Nav, Form, InputGroup, FormControl, Button, NavDropdown } from 'react-bootstrap'
 import { Line } from 'react-shapes'
 
 function Home() {
   return (
     <div>
-      <Navbar className="navBar">
+      <Navbar className="navBar" expand="md">
         <Navbar.Brand href="/">
           <img className="logoImg" alt="logo-img" type="image/png" src={Logo}/>
         </Navbar.Brand>
           <div className="Sign">
-          <Nav.Link className="Nav" href="/pricing">Pricing</Nav.Link>
-          <Form inline>
-            <InputGroup>
-              <FormControl
-                placeholder="Username"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </InputGroup>
-          </Form>
-          <Form inline>
-            <FormControl type="password" placeholder="Password" className=" mr-sm-2" />
-            <Button type="submit">SignUp/Login</Button>
-          </Form>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link className="Nav" href="/pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form className="forms-home" inline>
+              <InputGroup >
+                <FormControl
+                  placeholder="Username"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                />
+              </InputGroup>
+            </Form>
+            <Form className="forms-home" inline>
+              <FormControl  type="password" placeholder="Password" className=" mr-sm-2" />
+              <Button type="submit">SignUp/Login</Button>
+            </Form>
+          </Navbar.Collapse>
           </div>
+          
       </Navbar>
-      <Line x1={15} x2={550} y1={25} y2={350}  stroke={{color:'#E65243'}} strokeWidth={3} />
     </div>
   );
 }
--- client/src/Pricing.js
+++ client/src/Pricing.js
@@ -1,9 +1,32 @@
 import './App.css';
+import Logo from './img/server-logo.png'
 import React from 'react'
+import { Navbar, Nav, Form, InputGroup, Button, FormControl } from 'react-bootstrap'
 
 function Pricing() {
     return (
         <div>
+            <Navbar className="navBar">
+                <Navbar.Brand href="/">
+                <img className="logoImg" alt="logo-img" type="image/png" src={Logo}/>
+                </Navbar.Brand>
+                <div className="Sign">
+                <Nav.Link className="Nav" href="/pricing">Pricing</Nav.Link>
+                <Form inline>
+                    <InputGroup>
+                    <FormControl
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                    </InputGroup>
+                </Form>
+                <Form inline>
+                    <FormControl type="password" placeholder="Password" className=" mr-sm-2" />
+                    <Button type="submit">SignUp/Login</Button>
+                </Form>
+                </div>
+            </Navbar>
             <h1>This is Pricing</h1>
         </div>
     )
